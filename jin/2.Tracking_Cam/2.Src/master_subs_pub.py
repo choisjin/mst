efbddaac9ba1697735730ea0f3181f7efb0167dd
choisjin <choi3206@gmail.com>
2021-12-28 17:43:35 +0900

    ook

---
+++ jin/2.Tracking_Cam/2.Src/master_subs_pub.py
@@ -0,0 +1,65 @@
+#-*- coding:utf-8 -*-
+import sys
+import rospy
+import cv2
+import numpy as np
+from cv_bridge import CvBridge, CvBridgeError
+from sensor_msgs.msg import Image
+from sensor_msgs.msg import CompressedImage
+from std_msgs.msg import Int64MultiArray
+# from std_msgs.msg import String
+
+class Rasp_Cam_Subscriber():
+    def __init__(self):
+        
+        self.selecting_sub_image = "compressed" # 토픽선택 compressed or raw
+ 
+        if self.selecting_sub_image == "compressed":
+            self._sub = rospy.Subscriber('/usb_cam/image_raw/compressed', CompressedImage, self.callback, queue_size=1)
+        else:
+            self._sub = rospy.Subscriber('/usb_cam/image_raw', Image, self.callback, queue_size=1)
+ 
+            self.bridge = CvBridge()
+
+    def callback(self, image_msg):  
+ 
+        if self.selecting_sub_image == "compressed":
+            # 토픽데이터 cv용 데이터로 컨버팅
+            np_arr = np.fromstring(image_msg.data, np.uint8)
+            cv_image = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+        elif self.selecting_sub_image == "raw":
+            cv_image = self.bridge.imgmsg_to_cv2(image_msg, "bgr8")
+
+        faceCascade = cv2.CascadeClassifier('/home/jin/mst/jin/2.Tracking_Cam/2.Src/Data/haarcascade_frontalface_default.xml')
+
+        gray = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
+        faces = faceCascade.detectMultiScale(
+            gray,
+            scaleFactor=1.2,
+            minNeighbors=5,
+            minSize=(60, 60)
+        )
+        # 얼굴인식 후 사각형 그리기
+        for (x,y,w,h) in faces:
+            cv2.rectangle(cv_image,(x,y),(x+w,y+h),(255,0,0),2)
+            roi_gray = gray[y:y+h, x:x+w]
+            roi_color = cv_image[y:y+h, x:x+w]        
+
+            pub = rospy.Publisher('servo_x3', Int64MultiArray, queue_size=10)
+            my_msg = Int64MultiArray()
+            my_msg.data = [x,y,w,h]
+            pub.publish(my_msg)
+
+        # 이미지 출력
+        cv2.imshow('Face_Tracking', cv_image)
+        k = cv2.waitKey(1) & 0xff
+        
+    # 토픽 끊기지 않게 하기위한 스핀명령    
+    def main(self):
+        rospy.spin()
+
+# 클래스 내 함수 실행 
+if __name__ == '__main__':
+    rospy.init_node('Face_Tracking')
+    node = Rasp_Cam_Subscriber()
+    node.main()
--- jin/2.Tracking_Cam/2.Src/servo_subscriber.py
+++ jin/2.Tracking_Cam/2.Src/servo_subscriber.py
@@ -13,6 +13,9 @@ import Adafruit_PCA9685
 
 pwm = Adafruit_PCA9685.PCA9685() 
 pwm.set_pwm_freq(60) 
+midScreenX = (640/2)    # screen x mid
+midScreenY = (480/2)    # screen y mid
+midScreenWindow = 30    # correctio
 
 def set_servo_pulse(channel, pulse):
     pulse_length = 1000000    # 1,000,000 us per second
@@ -27,7 +30,6 @@ def set_servo_pulse(channel, pulse):
 class Servo_Subscriber():
     def __init__(self):
         self._sub_x = rospy.Subscriber('/servo_x3', Int64, self.callback, queue_size=1)
-        self._sub_y = rospy.Subscriber('/servo_y3', Int64, self.callback, queue_size=1)
         
         #print(self._sub_x)
         #print(type(self._sub_x))
@@ -35,11 +37,31 @@ class Servo_Subscriber():
         # print(type(int(self._sub_x)))
     
     def callback(self,servo_msg):
-        sub_x=servo_msg.data
+        servo_x = 320   # servo_x defalt position
+        servo_y = 390
+
+        for (x,y,w,h) in servo_msg.data:
+            servo_x1 = int(x+w/2)
+            servo_y1 = int(y+h/2)
+
+            if servo_x1 < midScreenX-midScreenWindow:
+                servo_x += 1    
+                pwm.set_pwm(1, 0, servo_x)                
+
+            elif servo_x1 > midScreenX+midScreenWindow:
+                servo_x -= 1
+                pwm.set_pwm(1, 0, servo_x)                                
+ 
+            if servo_y1 > midScreenY+midScreenWindow:
+                servo_y += 1
+                pwm.set_pwm(0, 0, servo_y)                           
+
+            elif servo_y1 < midScreenY-midScreenWindow:
+                servo_y -= 1
+                pwm.set_pwm(0, 0, servo_y)      
         #self._sub_y=int(self._sub_y)
-        print(sub_x)
-        print(type(sub_x))        
-        pwm.set_pwm(1, 0, sub_x)
+                
+        #pwm.set_pwm(1, 0, sub_x)
         #pwm.set_pwm(0, 0, int(self._sub_y))
     
     def main(self):
