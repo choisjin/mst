1daccec92d52efb5619bf2788b0522eba02b226b
choisjin <choi3206@gmail.com>
2022-01-14 12:07:32 +0900

    pi_multi

--- jin/The_latest_package/Raspberry_Pi/pi_ultiproccing.py
+++ jin/The_latest_package/Raspberry_Pi/pi_ultiproccing.py
@@ -47,7 +47,7 @@ class Cam_Publisher():
 class Servo_Subscriber():
     def __call__(self):
         rospy.init_node('servo_subs', anonymous=True)
-        self.manual_check_subs = rospy.Subscriber('/manual%s_check' % Camera_number, Int8, self.callback_checker, queue_size=1)
+        
         self.Servo_subs = rospy.Subscriber('/servo_controller_%s' % Camera_number, UInt16MultiArray, self.callback, queue_size=1)
         
         self.servo_x = 320
@@ -55,12 +55,17 @@ class Servo_Subscriber():
         pwm.set_pwm(1, 0, self.servo_x)
         pwm.set_pwm(0, 0, self.servo_y)
         
-        self.manual_checker = 1    
+        self.manual_checker = 0    
         
         rospy.spin()
    
     def callback(self, servo_msg):
-        if self.manual_checker == 1:        
+        self.manual_check_subs = rospy.Subscriber('/manual%s_check' % Camera_number, Int8, self.callback_checker, queue_size=1)
+
+        if self.manual_checker == 1:
+            self.manual_subs = rospy.Subscriber('/manual%s_axis' % Camera_number,  UInt16MultiArray, self.callback_manual_axis, queue_size=1)
+
+        elif self.manual_checker == 0:        
             x = servo_msg.data[0]
             y = servo_msg.data[1]
             w = servo_msg.data[2]
@@ -82,14 +87,11 @@ class Servo_Subscriber():
                 self.servo_y -= 1
                 pwm.set_pwm(0, 0, self.servo_y)
     
-            servo_axis_pub = rospy.Publisher('servo%s_axis' % Camera_number, Int8MultiArray, self.callback_manual_axis, queue_size=1)
-            servo_axis_msg = Int8MultiArray()
+            servo_axis_pub = rospy.Publisher('servo%s_axis' % Camera_number,  UInt16MultiArray, queue_size=1)
+            servo_axis_msg = UInt16MultiArray()
             servo_axis_msg.data = [self.servo_x, self.servo_y]
             servo_axis_pub.publish(servo_axis_msg)
         
-        elif self.manual_checker == 0:
-            self.manual_subs = rospy.Subscriber('/manual%s_axis' % Camera_number, Int8MultiArray, self.callback_manual_axis, queue_size=1)
-    
     def callback_checker(self, check_msg):
         self.manual_checker = check_msg.data
     
@@ -99,45 +101,38 @@ class Servo_Subscriber():
 
 class Manual_Subscriber():
     def __call__(self): 
-        rospy.init_node('manual_subs', anonymous=True)
-        self.manual_check_subs = rospy.Subscriber('/manual%s_check' % Camera_number, Int8, self.callback_checker, queue_size=1)
-        self._Manual_subs = rospy.Subscriber('/manual_control_%s' % Camera_number, Int8MultiArray, self.callback_manual, queue_size=1)
-        
-        self.servo_x = 320 
-        self.servo_y = 390
-        print('1')
         self.manual_checker = 0
-        
+        rospy.init_node('manual_subs', anonymous=True)
+        self.Manual_subs = rospy.Subscriber('/manual_control_%s' % Camera_number,  Int8MultiArray, self.callback_manual, queue_size=1)
+        self.servo_subs = rospy.Subscriber('/servo%s_axis' % Camera_number,  UInt16MultiArray, self.callback_servo_axis, queue_size=1)
         rospy.spin()
-    
+
     def callback_manual(self, manual_msg): 
-        if self.manual_checker == 1:
-            self.servo_subs = rospy.Subscriber('/servo%s_axis' % Camera_number, Int8MultiArray, self.callback_servo_axis, queue_size=1)       
+        self.manual_check_subs = rospy.Subscriber('/manual%s_check' % Camera_number, Int8, self.callback_checker, queue_size=1)
+
+        x = manual_msg.data[0]
+        y = manual_msg.data[1]    
+        self.servo_x1 = x
+        self.servo_y1 = y
         
-        elif self.manual_checker == 0:
-            x = manual_msg.data[0]
-            y = manual_msg.data[1]    
-            self.servo_x1 = x
-            self.servo_y1 = y
+        if self.servo_x1 == 1:
+            self.servo_x += 1
+            pwm.set_pwm(1, 0, self.servo_x)
+        elif self.servo_x1 == -1:
+            self.servo_x -= 1
+            pwm.set_pwm(1, 0, self.servo_x)
+        elif self.servo_y1 == 1:
+            self.servo_y -= 1
+            pwm.set_pwm(0, 0, self.servo_y)
+        elif self.servo_y1 == -1:
+            self.servo_y += 1
+            pwm.set_pwm(0, 0, self.servo_y)
+        
+        manual_axis_pub = rospy.Publisher('manual%s_axis' % Camera_number,  UInt16MultiArray, queue_size=1)
+        manual_axis_msg =  UInt16MultiArray()
+        manual_axis_msg.data = [self.servo_x, self.servo_y]
+        manual_axis_pub.publish(manual_axis_msg)
 
-            if self.servo_x1 == 1:
-                self.servo_x += 1
-                pwm.set_pwm(1, 0, self.servo_x)
-            elif self.servo_x1 == -1:
-                self.servo_x -= 1
-                pwm.set_pwm(1, 0, self.servo_x)
-            elif self.servo_y1 == 1:
-                self.servo_y -= 1
-                pwm.set_pwm(0, 0, self.servo_y)
-            elif self.servo_y1 == -1:
-                self.servo_y += 1
-                pwm.set_pwm(0, 0, self.servo_y)
-
-            manual_axis_pub = rospy.Publisher('manual%s_axis' % Camera_number, Int8MultiArray, self.callback_servo_axis, queue_size=1)
-            manual_axis_msg = Int8MultiArray()
-            manual_axis_msg.data = [self.servo_x, self.servo_y]
-            manual_axis_pub.publish(manual_axis_msg)
-    
     def callback_checker(self, check_msg):
         self.manual_checker = check_msg.data
 
